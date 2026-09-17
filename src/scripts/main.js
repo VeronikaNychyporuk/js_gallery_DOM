ae03281e9e10b4f7ba50d09ea7a7d6dba7ae0f27
Veronika Nychyporuk <veronikanychyporuk@gmail.com>
2026-09-18 00:46:38 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const allImages = document.querySelectorAll('.list-item__link');
+const largeImage = document.querySelector('.gallery__large-img');
+
+for (const img of allImages) {
+  img.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    largeImage.src = img.href;
+  });
+}
